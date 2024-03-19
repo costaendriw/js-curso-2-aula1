51fdf5d3a6ab7de3af23cc8b9c5474114f2ff0e7
Endriw Costa <endriw.costa3@gmail.com>
2024-03-19 19:26:11 -0300

    Interagindo com HTML

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = ' Jogo do número secreto ';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Digite um número de 1 a 100:';
+
+function verificarChute() {
+    console.log('O botão foi clicado!');
+}
